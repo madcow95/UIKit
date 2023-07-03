a9e773ef1c956c105fc31e9264ebd24f76107d63
madcow95 <lobasketve@naver.com>
2023-07-03 21:27:53 +0900

    Feat : 1000자리 수 이상 보기쉽게 convert, diff가 양수/음수에 따라서 색 변경
    1. 1000자리수 이상의 항목에 대해 1,000와 같이 수정
    2. diff가 양수일 경우 빨간색, 음수일 경우 파란색으로 표시

--- StockRank/StockRank/StockRankCollectionViewCell.swift
+++ StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -19,7 +19,25 @@ class StockRankCollectionViewCell: UICollectionViewCell {
         rankLabel.text = "\(stock.rank)"
         companyIconImageView.image = UIImage(named: stock.imageName)
         companyNameLabel.text = stock.name
-        stockPriceLabel.text = "\(stock.price) 원"
+        
+        let convertedPrice = convertToCurrencyFormat(price: stock.price)
+        stockPriceLabel.text = "\(convertedPrice) 원"
+        
+        let diffLabelColor = stock.diff > 0 ? UIColor.red : UIColor.blue
         diffLabel.text = "\(stock.diff)%"
+        diffLabel.textColor = diffLabelColor
+    }
+    
+    func convertToCurrencyFormat(price: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        // 몇자리 소수점까지 표시할 것인지
+        // default : 0
+        // 3을 입력하면 소수점 3번째 자리까지 출려
+        numberFormatter.maximumFractionDigits = 0
+        guard let priceToString = numberFormatter.string(from: NSNumber(value: price)) else {
+            return ""
+        }
+        return priceToString
     }
 }
